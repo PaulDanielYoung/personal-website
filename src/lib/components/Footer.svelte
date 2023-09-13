6e4045a4937d921f915d7f37c5e967b80a36edf8
Paul Young <pauldanielyoung@outlook.com>
2023-09-13 00:09:31 -0400

    Updated footer icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,49 +1,26 @@
+<script lang="ts">
+	import { IconBrandLinkedin } from '@tabler/icons-svelte';
+	import { IconBrandGithub } from '@tabler/icons-svelte';
+	import { IconAt } from '@tabler/icons-svelte';
+</script>
+
 <footer class="bg-white">
-	<div class="mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-between lg:px-8">
+	<div class="mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-between lg:px-10">
 		<div class="flex justify-center space-x-6 md:order-2">
 			<a href="#" class="text-gray-400 hover:text-gray-500">
 				<span class="sr-only">LinkedIn</span>
-				<svg
-					id="footersvg"
-					xmlns="http://www.w3.org/2000/svg"
-					width="32"
-					height="32"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="h-6 w-6"
-					><path
-						d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-					/><rect x="2" y="9" width="4" height="12" /><circle cx="4" cy="4" r="2" /></svg
-				>
+				<IconBrandLinkedin class="h-8 w-8" />
 			</a>
 			<a href="#" class="text-gray-400 hover:text-gray-500">
-				<span class="sr-only">Twitter</span>
-				<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-					<path
-						d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-					/>
-				</svg>
+				<span class="sr-only">Github</span>
+				<IconBrandGithub class="h-8 w-8" />
 			</a>
 			<a href="#" class="text-gray-400 hover:text-gray-500">
-				<span class="sr-only">GitHub</span>
-				<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-					<circle cx="12" cy="12" r="4" /><path
-						d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"
-					/>
-				</svg>
-			</a>
-			<a href="#" class="text-gray-400 hover:text-gray-500">
-				<span class="sr-only">YouTube</span>
-				<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-					<path
-						fill-rule="evenodd"
-						d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-						clip-rule="evenodd"
-					/>
-				</svg>
+				<span class="sr-only">Email</span>
+				<IconAt class="h-8 w-8" />
 			</a>
 		</div>
-		<div class="mt-8 md:order-1 md:mt-0">
+		<div class="mt-10 md:order-1 md:mt-0">
 			<p class="text-center text-xs leading-5 text-gray-500">
 				&copy; 2023 Paul Young. All rights reserved.
 			</p>
