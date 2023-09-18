08aa5a6af7edfe1abc127966e5517d2bf340c77c
Paul Young <pauldanielyoung@outlook.com>
2023-09-18 03:11:04 -0400

    Layout page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
 	import '../app.postcss';
+	import PageTransition from '$lib/components/Transition.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+
+	export let data;
 </script>
 
 <div class="flex min-h-screen flex-col">
 	<div class="flex-grow">
-		<slot />
+		<PageTransition url={data.url}>
+			<slot />
+		</PageTransition>
 	</div>
 	<Footer />
 </div>
