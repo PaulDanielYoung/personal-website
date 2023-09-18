e5c232cf5d79b01722e49d139f5834ba74c64a86
Paul Young <pauldanielyoung@outlook.com>
2023-09-18 07:31:35 -0400

    Main blog formatting

--- src/lib/components/Blog.svelte
+++ src/lib/components/Blog.svelte
@@ -1,33 +1,32 @@
-<div class="my-12">
-	<div
-		class="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 border-8 px-6 sm:gap-y-16 lg:grid-cols-2 lg:px-8"
-	>
-		<article class="mx-auto w-full max-w-2xl border-4 border-red-500 lg:mx-0 lg:max-w-lg">
-			<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600">Mar 16, 2020</time>
-			<h2
-				id="featured-post"
-				class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
-			>
-				We’re incredibly proud to announce we have secured $75m in Series B
-			</h2>
-			<p class="mt-4 text-lg leading-8 text-gray-600">
-				Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id.
-				Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc.
-				Nunc netus sit faucibus.
-			</p>
-			<div
-				class="mt-4 flex flex-col justify-between gap-6 sm:mt-8 sm:flex-row-reverse sm:gap-8 lg:mt-4 lg:flex-col"
-			>
-				<div class="flex">
-					<a
-						href="#"
-						class="text-sm font-semibold leading-6 text-indigo-600"
-						aria-describedby="featured-post"
-						>Continue reading <span aria-hidden="true">&rarr;</span></a
-					>
-				</div>
-				<div class="flex items-end justify-between lg:border-t lg:border-gray-900/10 lg:pt-8">
-					<a href="#" class="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
+<div class="my-12 px-6 lg:px-8">
+	<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 lg:max-w-none lg:grid-cols-2">
+		<article class="flex h-full flex-col items-center justify-center border-b-2 lg:border-b-0">
+			<div class="flex flex-col space-y-4">
+				<!-- This wrapper is added to ensure the spacing between the child elements remains consistent -->
+				<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600">Mar 16, 2020</time
+				>
+				<h2
+					id="featured-post"
+					class="mt-4 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl xl:text-3xl"
+				>
+					We’re incredibly proud to announce we have secured $75m in Series B
+				</h2>
+				<p class="text-md mt-4 leading-6 text-gray-600 xl:text-lg">
+					Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid
+					explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel
+					iusto corrupti dicta laboris incididunt.
+				</p>
+				<div class="flex flex-col justify-between gap-4 lg:flex-col">
+					<div class="flex justify-between">
+						<a
+							href="#"
+							class="text-sm font-semibold leading-6 text-indigo-600"
+							aria-describedby="featured-post"
+							>Continue reading <span aria-hidden="true">&rarr;</span></a
+						>
+						<span class="px-6 text-sm text-gray-500">5 min read</span>
+					</div>
+					<div class="mb-4 flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
 						<span
 							class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
 							>Badge</span
@@ -40,103 +39,101 @@
 							class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
 							>Badge</span
 						>
-					</a>
-					<span class="text-sm text-gray-500">5 min read</span>
+					</div>
 				</div>
 			</div>
 		</article>
-		<div
-			class="mx-auto w-full max-w-2xl border-t border-gray-900/10 pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0"
-		>
-			<div class="-my-12 divide-y divide-gray-900/10">
-				<article class="py-12">
-					<div class="group relative max-w-xl">
-						<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600"
-							>Mar 10, 2020</time
-						>
-						<div class="flex justify-between">
-							<h2 class="mt-2 text-lg font-semibold text-gray-900 group-hover:text-gray-600">
-								<a href="#">
-									<span class="absolute inset-0"></span>
-									Boost your conversion rate
-								</a>
-							</h2>
-							<a href="#" class="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
-								<span
-									class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
-									>Badge</span
-								>
-								<span
-									class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
-									>Badge</span
-								>
-								<span
-									class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-									>Badge</span
-								>
-							</a>
-						</div>
-						<p class="mt-4 text-sm leading-6 text-gray-600">
-							Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
-							aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
-							deserunt vel iusto corrupti dicta laboris incididunt.
-						</p>
-					</div>
-					<div class="mt-4 flex justify-between">
-						<a
-							href="#"
-							class="text-sm font-semibold leading-6 text-indigo-600"
-							aria-describedby="featured-post"
-							>Continue reading <span aria-hidden="true">&rarr;</span></a
-						>
 
-						<span class="text-sm text-gray-500">5 min read</span>
-					</div>
-				</article>
-
-				<article class="py-12">
-					<div class="group relative max-w-xl">
-						<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600"
-							>Mar 10, 2020</time
-						>
-						<h2 class="mt-2 text-lg font-semibold text-gray-900 group-hover:text-gray-600">
-							<a href="#">
-								<span class="absolute inset-0"></span>
-								Boost your conversion rate
-							</a>
-						</h2>
-						<p class="mt-4 text-sm leading-6 text-gray-600">
-							Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
-							aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
-							deserunt vel iusto corrupti dicta laboris incididunt.
-						</p>
-					</div>
-					<div class="mt-4 flex justify-between">
-						<a
-							href="#"
-							class="text-sm font-semibold leading-6 text-indigo-600"
-							aria-describedby="featured-post"
-							>Continue reading <span aria-hidden="true">&rarr;</span></a
-						>
-						<a href="#" class="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
-							<span
-								class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
-								>Badge</span
-							>
-							<span
-								class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
-								>Badge</span
-							>
-							<span
-								class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-								>Badge</span
-							>
+		<div class="mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-none">
+			<article class="border-b-2">
+				<div class="group relative max-w-3xl pt-4">
+					<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600"
+						>Mar 10, 2020</time
+					>
+					<h2 class="mt-2 text-lg font-semibold text-gray-900 group-hover:text-gray-600">
+						<a href="#">
+							<span class="absolute inset-0"></span>
+							Boost your conversion rate
 						</a>
-						<span class="text-sm text-gray-500">5 min read</span>
+					</h2>
+					<p class="mt-4 text-sm leading-6 text-gray-600">
+						Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
+						aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
+						deserunt vel iusto corrupti dicta laboris incididunt.
+					</p>
+				</div>
+				<div class="mt-3 flex flex-col justify-between gap-4 lg:flex-col">
+					<div class="flex justify-between">
+						<a
+							href="#"
+							class="text-sm font-semibold leading-6 text-indigo-600"
+							aria-describedby="featured-post"
+							>Continue reading <span aria-hidden="true">&rarr;</span></a
+						>
+						<span class="px-6 text-sm text-gray-500">5 min read</span>
 					</div>
-				</article>
-				<!-- More posts... -->
-			</div>
+					<div class="mb-4 flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
+						<span
+							class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+							>Badge</span
+						>
+						<span
+							class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
+							>Badge</span
+						>
+						<span
+							class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+							>Badge</span
+						>
+					</div>
+				</div>
+			</article>
+
+			<article>
+				<div class="group relative max-w-3xl pt-4">
+					<time datetime="2020-03-16" class="block text-sm leading-6 text-gray-600"
+						>Mar 10, 2020</time
+					>
+					<h2 class="mt-2 text-lg font-semibold text-gray-900 group-hover:text-gray-600">
+						<a href="#">
+							<span class="absolute inset-0"></span>
+							Boost your conversion rate
+						</a>
+					</h2>
+					<p class="mt-4 text-sm leading-6 text-gray-600">
+						Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
+						aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
+						deserunt vel iusto corrupti dicta laboris incididunt.
+					</p>
+				</div>
+				<div class="mt-3 flex flex-col justify-between gap-4 lg:flex-col">
+					<div class="flex justify-between">
+						<a
+							href="#"
+							class="text-sm font-semibold leading-6 text-indigo-600"
+							aria-describedby="featured-post"
+							>Continue reading <span aria-hidden="true">&rarr;</span></a
+						>
+						<span class="px-6 text-sm text-gray-500">5 min read</span>
+					</div>
+					<div class="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900">
+						<span
+							class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+							>Badge</span
+						>
+						<span
+							class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
+							>Badge</span
+						>
+						<span
+							class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+							>Badge</span
+						>
+					</div>
+				</div>
+			</article>
+
+			<!-- More posts... -->
 		</div>
 	</div>
 </div>
