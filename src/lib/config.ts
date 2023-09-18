f875fd6b6d58b67222c476ffc3e08cd0ca7f18f6
Paul Young <pauldanielyoung@outlook.com>
2023-09-18 02:43:11 -0400

    Blog working

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Paul Young';
+export const description = 'A blog for my thoughts';
+export const url = dev ? 'http://localhost:5173/' : 'https://paulyoung.fun/';
