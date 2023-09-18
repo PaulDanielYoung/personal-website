ec31faf0428b8177e91e245e16947332381872cf
Paul Young <pauldanielyoung@outlook.com>
2023-09-17 23:49:57 -0400

    Updated projects section to include row-span-2

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -51,11 +51,11 @@
 			<p class="text-center">This is a description.</p>
 		</div>
 	</div>
-	<div class="self-center">
+	<div class="self-center lg:row-span-2">
 		<img
 			src="https://source.unsplash.com/random/?arts"
 			alt=""
-			class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+			class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[2/3]"
 		/>
 		<div class="py-2">
 			<p class="text-center">This is a description.</p>
@@ -64,6 +64,26 @@
 	<div class="lg:col-span-2">
 		<img
 			src="https://source.unsplash.com/random/?film"
+			alt=""
+			class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+		/>
+		<div class="py-2">
+			<p class="text-center">This is a description.</p>
+		</div>
+	</div>
+	<div class="self-center">
+		<img
+			src="https://source.unsplash.com/random/?architecture"
+			alt=""
+			class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+		/>
+		<div class="py-2">
+			<p class="text-center">This is a description.</p>
+		</div>
+	</div>
+	<div class="self-center">
+		<img
+			src="https://source.unsplash.com/random/?textures"
 			alt=""
 			class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
 		/>
