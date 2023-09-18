814debb82014ead3de64075a8a2ffe145bff2264
Paul Young <pauldanielyoung@outlook.com>
2023-09-18 03:34:16 -0400

    Error page is responsive and includes footer

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex items-center justify-center" style="min-height: calc(100vh - var(--error-offset))">
+	<h1 class="text-4xl font-semibold text-blue-500">
+		{$page.status}: {$page.error?.message}
+	</h1>
+</div>
+
+<style lang="postcss">
+	/* Base size */
+	:root {
+		--error-offset: 100px;
+	}
+
+	/* Medium screen */
+	@media (min-width: 768px) {
+		:root {
+			--error-offset: 68px;
+		}
+	}
+</style>
