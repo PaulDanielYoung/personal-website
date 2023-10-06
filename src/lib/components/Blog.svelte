c5855db643c9fe1cc657bdb9c3f3671f8abe0ff6
Paul Young <pauldanielyoung@outlook.com>
2023-10-05 22:13:16 -0400

    Main page blog formatting

--- src/lib/components/Blog.svelte
+++ src/lib/components/Blog.svelte
@@ -6,19 +6,6 @@
 	const featuredPost = posts[0];
 	const secondPost = posts[1];
 	const thirdPost = posts[2];
-
-	function getBgColor(index: number): string {
-		switch (index % 3) {
-			case 0:
-				return 'bg-red-50 text-red-700 ring-red-600/10';
-			case 1:
-				return 'bg-blue-50 text-blue-700 ring-blue-600/10';
-			case 2:
-				return 'bg-green-50 text-green-700 ring-green-600/10';
-			default:
-				return 'bg-red-50 text-red-700 ring-red-600/10';
-		}
-	}
 </script>
 
 <div class="mx-2 lg:mx-8">
@@ -29,28 +16,28 @@
 			<article
 				class="h-full rounded-xl border-4 bg-slate-50 p-3 lg:flex lg:transform lg:flex-col lg:p-6 lg:transition lg:hover:scale-101 lg:hover:shadow-xl"
 			>
-				<div class="relative">
-					<div class="flex items-center justify-between text-xs lg:text-sm">
-						<time datetime={featuredPost.date} class="text-gray-500">
-							{formatDate(featuredPost.date)}
-						</time>
+				<div class="relative mt-1 lg:mt-0">
+					<div class="flex items-center justify-between text-xs">
+						<time
+							datetime={featuredPost.date}
+							class="block text-xs leading-6 text-gray-600 lg:text-sm"
+							>{formatDate(featuredPost.date)}</time
+						>
 						<div class="space-x-2">
 							{#each featuredPost.categories as category}
 								<span
-									class="relative z-10 rounded-full bg-indigo-50 px-3 py-1.5 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10"
+									class="relative z-10 rounded-full bg-indigo-50 px-3 py-1 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 lg:py-1.5"
 								>
 									{category}
 								</span>
 							{/each}
 						</div>
 					</div>
-					<h2
-						class="mt-2 line-clamp-2 text-lg font-semibold text-gray-900 lg:mt-6 lg:text-3xl xl:text-4xl"
-					>
+					<h2 class="mt-2 line-clamp-2 text-base font-semibold text-gray-900 lg:mt-6 lg:text-3xl">
 						{featuredPost.title}
 					</h2>
 					<p
-						class="mt-2 line-clamp-3 text-sm leading-6 text-gray-600 lg:mt-6 lg:line-clamp-8 lg:text-base xl:text-base"
+						class="mt-2 line-clamp-3 text-xs leading-6 text-gray-600 lg:mt-6 lg:line-clamp-8 lg:text-base"
 					>
 						{featuredPost.description}
 					</p>
@@ -58,10 +45,10 @@
 
 				<div class="mt-3 flex flex-col justify-end gap-4 lg:mt-auto">
 					<div class="flex items-center justify-between">
-						<span class="text-sm font-semibold leading-6 text-indigo-600 lg:text-base">
+						<span class="text-xs font-semibold leading-6 text-indigo-600 lg:text-base">
 							Continue reading <span aria-hidden="true">&rarr;</span>
 						</span>
-						<span class="text-sm text-gray-500 lg:text-base">{featuredPost.readingLength}</span>
+						<span class="text-xs text-gray-500 lg:text-base">{featuredPost.readingLength}</span>
 					</div>
 				</div>
 			</article>
@@ -74,32 +61,34 @@
 				>
 					<div class="relative">
 						<div class="flex items-center justify-between text-xs">
-							<time datetime={secondPost.date} class="block text-sm leading-6 text-gray-600"
+							<time
+								datetime={secondPost.date}
+								class="block text-xs leading-6 text-gray-600 lg:text-sm"
 								>{formatDate(secondPost.date)}</time
 							>
 							<div class="space-x-2">
 								{#each secondPost.categories as category}
 									<span
-										class="relative z-10 rounded-full bg-indigo-50 px-3 py-1.5 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10"
+										class="relative z-10 rounded-full bg-indigo-50 px-3 py-1 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 lg:py-1.5"
 									>
 										{category}
 									</span>
 								{/each}
 							</div>
 						</div>
-						<h2 class="mt-2 line-clamp-2 text-lg font-semibold text-gray-900">
+						<h2 class="mt-2 line-clamp-2 text-base font-semibold text-gray-900 lg:text-lg">
 							{secondPost.title}
 						</h2>
-						<p class="mt-2 line-clamp-3 text-sm leading-6 text-gray-600">
+						<p class="mt-2 line-clamp-3 text-xs leading-6 text-gray-600 lg:text-sm">
 							{secondPost.description}
 						</p>
 					</div>
 					<div class="mt-3 flex flex-col justify-between gap-4 lg:flex-col">
 						<div class="flex items-center justify-between">
-							<span class="text-sm font-semibold leading-6 text-indigo-600"
+							<span class="text-xs font-semibold leading-6 text-indigo-600 lg:text-sm"
 								>Continue reading <span aria-hidden="true">&rarr;</span></span
 							>
-							<span class="text-sm text-gray-500">{secondPost.readingLength}</span>
+							<span class="text-xs text-gray-500 lg:text-sm">{secondPost.readingLength}</span>
 						</div>
 					</div>
 				</article>
@@ -111,32 +100,34 @@
 				>
 					<div class="relative">
 						<div class="flex items-center justify-between text-xs">
-							<time datetime={thirdPost.date} class="block text-sm leading-6 text-gray-600"
+							<time
+								datetime={thirdPost.date}
+								class="block text-xs leading-6 text-gray-600 lg:text-sm"
 								>{formatDate(thirdPost.date)}</time
 							>
 							<div class="space-x-2">
 								{#each thirdPost.categories as category}
 									<span
-										class="relative z-10 rounded-full bg-indigo-50 px-3 py-1.5 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10"
+										class="relative z-10 rounded-full bg-indigo-50 px-3 py-1 font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 lg:py-1.5"
 									>
 										{category}
 									</span>
 								{/each}
 							</div>
 						</div>
-						<h2 class="mt-2 line-clamp-2 text-lg font-semibold text-gray-900">
+						<h2 class="mt-2 line-clamp-2 text-base font-semibold text-gray-900 lg:text-lg">
 							{thirdPost.title}
 						</h2>
-						<p class="mt-2 line-clamp-3 text-sm leading-6 text-gray-600">
+						<p class="mt-2 line-clamp-3 text-xs leading-6 text-gray-600 lg:text-sm">
 							{thirdPost.description}
 						</p>
 					</div>
 					<div class="mt-3 flex flex-col justify-between gap-4 lg:flex-col">
 						<div class="flex items-center justify-between">
-							<span class="text-sm font-semibold leading-6 text-indigo-600"
+							<span class="text-xs font-semibold leading-6 text-indigo-600 lg:text-sm"
 								>Continue reading <span aria-hidden="true">&rarr;</span></span
 							>
-							<span class="text-sm text-gray-500">{thirdPost.readingLength}</span>
+							<span class="text-xs text-gray-500 lg:text-sm">{thirdPost.readingLength}</span>
 						</div>
 					</div>
 				</article>
