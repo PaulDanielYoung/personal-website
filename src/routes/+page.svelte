de90c68b897b3c342743ca529b24812d2e510b9b
Paul Young <pauldanielyoung@outlook.com>
2025-07-11 15:38:30 -0700

    Updated mobile formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <div class="min-h-screen flex items-start justify-center bg-neutral-50">
-	<p class="mt-32 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-extrabold text-gray-700 font-sans leading-tight text-center">
+	<p class="mt-32 text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-extrabold text-gray-700 font-sans leading-tight text-center">
 		paul young likes to write <a href="https://github.com/PaulDanielYoung" class="text-blue-600 underline font-extrabold" target="_blank" rel="noopener noreferrer">code</a><br />
 		and check his <a href="mailto:pauldanielyoung@outlook.com" class="text-blue-600 underline font-extrabold">email</a>.
 	</p>
