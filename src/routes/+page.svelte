2db614af0ba63e2d5de07bb8b523c17d80cd67b9
Paul Young <pauldanielyoung@outlook.com>
2025-07-11 13:23:16 -0700

    Main Page Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>Paul Young - Personal Website</title>
+</svelte:head>
+
+<div class="min-h-screen flex items-start justify-center bg-white">
+	<p class="mt-32 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-extrabold text-gray-700 font-sans leading-tight text-center">
+		paul young likes to write <a href="https://github.com/PaulDanielYoung" class="text-blue-600 underline font-extrabold" target="_blank" rel="noopener noreferrer">code</a><br />
+		and check his <a href="mailto:pauldanielyoung@outlook.com" class="text-blue-600 underline font-extrabold">email</a>.
+	</p>
+</div>
